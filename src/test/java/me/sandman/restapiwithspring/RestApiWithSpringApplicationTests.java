be6bdace7a31b176c52bb3e780118805904c15f1
lch131 <lch131@jiran.com>
2019-06-12 21:04:14 +0900

    project created.

---
+++ src/test/java/me/sandman/restapiwithspring/RestApiWithSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package me.sandman.restapiwithspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestApiWithSpringApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
