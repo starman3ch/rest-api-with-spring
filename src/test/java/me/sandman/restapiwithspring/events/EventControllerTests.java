d281927c43291daf3910c2200ce632349c88af7b
DESKTOP-MGIULOS\starman <starman3ch@gmail.com>
2019-06-16 23:58:00 +0900

    Add test description annotation

--- src/test/java/me/sandman/restapiwithspring/events/EventControllerTests.java
+++ src/test/java/me/sandman/restapiwithspring/events/EventControllerTests.java
@@ -2,6 +2,7 @@ package me.sandman.restapiwithspring.events;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import me.sandman.restapiwithspring.common.TestDescription;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -38,6 +39,7 @@ public class EventControllerTests {
 
 
     @Test
+    @TestDescription("정상적으로 이벤트를 생성하는 테스트")
     public void createEvent() throws Exception {
         EventDto eventDto = EventDto.builder()
                 .name("Spring")
@@ -69,6 +71,7 @@ public class EventControllerTests {
 
 
     @Test
+    @TestDescription("입력 받을 수 없는 값을 사용한 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request() throws Exception {
         Event event = Event.builder()
                 .id(100)
@@ -98,6 +101,7 @@ public class EventControllerTests {
 
 
     @Test
+    @TestDescription("입력 값이 비어있는 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request_Empty_Input() throws Exception {
         EventDto eventDto = EventDto.builder().build();
 
@@ -109,6 +113,7 @@ public class EventControllerTests {
 
 
     @Test
+    @TestDescription("입력 값이 잘못된 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request_Wrong_Input() throws Exception {
         EventDto eventDto = EventDto.builder()
                 .name("Spring")
