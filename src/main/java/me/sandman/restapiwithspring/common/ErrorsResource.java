83604edac88781b6fab2b2afbcfd7fc6c441fd8f
DESKTOP-MGIULOS\starman <starman3ch@gmail.com>
2019-07-03 18:11:31 +0900

    Create API Index

---
+++ src/main/java/me/sandman/restapiwithspring/common/ErrorsResource.java
@@ -0,0 +1,16 @@
+package me.sandman.restapiwithspring.common;
+
+import me.sandman.restapiwithspring.index.IndexController;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.Resource;
+import org.springframework.validation.Errors;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+public class ErrorsResource extends Resource<Errors> {
+    public ErrorsResource(Errors content, Link... links) {
+        super(content, links);
+        add(linkTo(methodOn(IndexController.class).index()).withRel("index"));
+    }
+}
