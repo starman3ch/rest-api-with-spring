b85cebbb1990bde7de2be0161b2f745a170392d3
DESKTOP-MGIULOS\starman <starman3ch@gmail.com>
2019-07-22 00:23:49 +0900

    API for Current User

--- src/main/java/me/sandman/restapiwithspring/accounts/AccountService.java
+++ src/main/java/me/sandman/restapiwithspring/accounts/AccountService.java
@@ -32,13 +32,7 @@ public class AccountService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Account account = accountRepository.findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException(username));
-        return new User(account.getEmail(), account.getPassword(), authorities(account.getRoles()));
-    }
-
-    private Collection<? extends GrantedAuthority> authorities(Set<AccountRole> roles) {
-        return roles.stream()
-                .map(r -> new SimpleGrantedAuthority("ROLE_" + r.name()))
-                .collect(Collectors.toSet());
+        return new AccountAdapter(account);
     }
 
 }
